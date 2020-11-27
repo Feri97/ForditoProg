cce04b2e1935c8c76fa941f272e068784eb411be
Csongrádi Ferenc Péter <45463205+Feri97@users.noreply.github.com>
2020-11-27 15:57:49 +0100

    FordProg Házik

---
+++ finitAutomata/finitAutomata/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace finitAutomata
+{
+    class Program
+    {
+
+        class Automata
+        {
+            string state = "A";
+            Dictionary<String, String> D = new Dictionary<String, String>();
+
+            public void setToDefault()
+            {
+                D.Clear();
+            }
+
+            public void prepare()
+            {
+                D.Add("A+", "B");
+                D.Add("A-", "B");
+                D.Add("Ad", "C");
+                D.Add("Bd", "C");
+                D.Add("Cd", "C");
+            }
+
+            public char convert(char c)
+            {
+                if (Char.IsDigit(c))
+                {
+                    return 'd';
+                }
+                return c;
+            }
+
+            public string delta(string st, char akt)
+            {
+                if (D.ContainsKey(st + convert(akt)))
+                {
+                    return D[st + convert(akt)];
+                }
+                return "Error";
+            }
+
+            public void main(string input)
+            {
+                prepare();
+                int i = 0;
+                while (i < input.Length && state != "Error")
+                {
+                    state = delta(state, input[i]);
+                    i++;
+                }
+
+                if (state != "Error")
+                {
+                    Console.WriteLine("{0} helyes bemenet", input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} helytelen bemenő adat. Hibás karakter található a {1}. helyen", input, i);
+                }
+                state = "A";
+                i = 0;
+                setToDefault();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            StreamReader sr = new StreamReader("teszt.txt");
+            string seged = "";
+            while (!sr.EndOfStream)
+            {
+                seged += sr.ReadLine();
+            }
+            String[] array = seged.Split(' ');
+            Automata A = new Automata();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                A.main(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ recAutomata/recAutomata/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace recAutomata
+{
+    class Automata
+    {
+        string input;
+        int i = 0;
+
+        public string simple(string st)
+        {
+            //return Regex.Replace(st, "[0-9]", "i");
+            return Regex.Replace(st, @"([0-9]+)", "i");
+        }
+
+        public Automata(string input)
+        {
+            Console.WriteLine("Eredeti input: {0}", input);
+            this.input = $"{simple(input)}#";
+            Console.WriteLine("Egyszerűsített input: {0}", this.input);
+        }
+
+        public void elfogad(char ch)
+        {
+            if (input[i] != ch)
+            {
+                Console.WriteLine("Hibás kifejezés: {0}. Helytelen karakter: {1}", input, input[i]);
+            }
+            i++;
+        }
+
+        public void S()
+        {
+            E();
+            elfogad('#');
+            Console.WriteLine("Az elemzés lefutott");
+        }
+
+        void E()
+        {
+            T();
+            Ev();
+        }
+
+        void T()
+        {
+            F();
+            Tv();
+        }
+
+        void Ev()
+        {
+            if (input[i] == '+')
+            {
+                elfogad('+');
+                T();
+                Ev();
+            }
+        }
+
+        void Tv()
+        {
+            if (input[i] == '*')
+            {
+                elfogad('*');
+                F();
+                Tv();
+            }
+        }
+
+        void F()
+        {
+            if (input[i] == '(')
+            {
+                elfogad('(');
+                E();
+                elfogad(')');
+            }
+            else
+            {
+                elfogad('i');
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automata A = new Automata("34*(231+345)");
+            A.S();
+            Console.ReadLine();
+        }
+    }
+}
